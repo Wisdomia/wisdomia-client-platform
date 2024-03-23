a0bb15f343520ce1f94740f7eb910036865af36e
nikola-bozin-org <nikola.bozin.org@gmail.com>
2024-03-23 15:28:47 +0100

    dev done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
+'use client'
 export default function Home() {
+  const handleClick = () => {
+    window.open("link", '_blank');
+  };
+
   return (
-   <div>In Development</div>
+   <div className={"home"}>
+    <h1>Wisdomia</h1>
+    <p>Platform in development!</p>
+    <p className="smallToBig" style={{marginTop:'100px'}}>Everything big starts with a small step.</p>
+    <p>Grab your access to the platform:</p>
+    <button className={`${"customButton"}`} onClick={handleClick}>
+      <strong>
+        Buy Now
+      </strong>
+    </button>
+   </div>
   );
 }
